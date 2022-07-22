9943ee2776b5ac96ffdd2e093c599a10f990025d
mike-snowflake-training <109202381+mike-snowflake-training@users.noreply.github.com>
2022-07-22 17:00:28 +1000

    Update streamlit_app.py
    added new table and fruit selector

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,6 +39,10 @@ import snowflake.connector
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("SELECT * FROM FRUIT_LOAD_LIST")
-my_data_row = my_cur.fetchone()
-streamlit.text("The fruit load list contains:")
-streamlit.text(my_data_row)
+my_data_rows = my_cur.fetchall()
+streamlit.header("The fruit load list contains:")
+streamlit.dataframe(my_data_rows)
+
+# Allow the end user to add a fruit to the list
+add_my_fruit = streamlit.text_input('What fruit would you like to add?', 'jackfruit')
+streamlit.write('Thanks for adding ', add_my_fruit)
